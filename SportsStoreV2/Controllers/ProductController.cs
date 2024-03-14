ad7dbcd132e2efa4ececce816f87f619d9697cd3
Kamil Sabirov <ksabirov448@gmail.com>
2024-03-14 22:52:44 +0300

    Добавление контроллера Product, а так же извлечение репозитория (ложного) из сервисов

---
+++ SportsStoreV2/Controllers/ProductController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStoreV2.Models;
+
+namespace SportsStoreV2.Controllers
+{
+	public class ProductController : Controller
+	{
+		private IProductRepository repository;
+		public ProductController(IProductRepository repository)
+		{
+			this.repository = repository;
+		}
+	}
+}
