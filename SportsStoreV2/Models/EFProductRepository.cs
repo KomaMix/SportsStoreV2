e81d58202932cb0ed25f62c332dc48e375da1b14
Kamil Sabirov <ksabirov448@gmail.com>
2024-03-16 11:12:21 +0300

    Создание репозитория EFProductRepository, а так же настройка конфигурации

---
+++ SportsStoreV2/Models/EFProductRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace SportsStoreV2.Models
+{
+	public class EFProductRepository : IProductRepository
+	{
+		private ApplicationDbContext context;
+		public EFProductRepository(ApplicationDbContext context)
+		{
+			this.context = context;
+		}
+		public IQueryable<Product> Products => context.Products;
+	}
+}
--- SportsStoreV2/Program.cs
+++ SportsStoreV2/Program.cs
@@ -1,10 +1,15 @@
+using Microsoft.EntityFrameworkCore;
 using SportsStoreV2.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddTransient<IProductRepository, FakeProductRepository>();
+builder.Services.AddTransient<IProductRepository, EFProductRepository>();
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var options = new DbContextOptionsBuilder<ApplicationDbContext>().UseSqlServer(connectionString).Options;
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+options.UseSqlServer(connectionString));
 
 
 var app = builder.Build();
